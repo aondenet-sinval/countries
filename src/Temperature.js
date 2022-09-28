9176d3262be2e6bea72a506d446f4b706f147628
aondenet-sinval <sinvalsatv@gmail.com>
2022-09-28 12:37:23 -0300

    add climatempo

--- src/Temperature.js
+++ src/Temperature.js
@@ -6,7 +6,7 @@ const Temperature = (props)=>{
   const [result,setResult] = useState([])
   const city = props.city
   const url = "https://api.openweathermap.org/data/2.5/"
-  const key = '3cd09b604a26a19ce507cc9f80c20ec1'
+  const key = process.env.REACT_APP_API_KEY
   console.log('key ', key);
   useEffect(() => {
   axios
@@ -17,13 +17,22 @@ const Temperature = (props)=>{
 
 console.log('isarray result ', Array.isArray(result));
 const temperature = [];
-temperature.push(result.main)
-const selectedTemperature = 'ee'
-// const selectedTemperature =
-  // temperature.map((weather, index) => <li key={index}>
-  //   <b>Temp. máxima:</b> {weather.temp_max}<br />
-  //   <b>Temp. mínima:</b> {/*weather.main.temp_min*/}<br />
-  //   </li>);
+result.main
+? temperature.push(result.main)
+: temperature.push({
+  humidity: 0,
+  temp: 0,
+  temp_max: 0,
+  temp_min: 0
+  })
+// const selectedTemperature = 'ee'
+const selectedTemperature =
+  temperature.map((weather, index) => <li key={index}>
+    <b>Umidade:</b> {weather.humidity}<br />
+    <b>Temperatura:</b> {weather.temp}<br />
+    <b>Temp. máxima:</b> {weather.temp_min}<br />
+    <b>Temp. mínima:</b> {weather.temp_min}<br />
+    </li>);
 
 console.log('result clima ', temperature);
   return(<ul>{selectedTemperature}</ul>)
